0b4ae8b03b44bb6e25ddb2e24d6302a5b38865fa
hafiz1993 <hafizurrahaman04@gmail.com>
2020-08-13 21:46:24 +0600

    trueflase

---
+++ truefalse.js
@@ -0,0 +1,27 @@
+
+// const age = 1;
+
+// if( age > 0){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+// Falsy
+// 0
+// ""
+// false
+// undefined
+// null
+// NaN
+// Truthy
+// '0' '' []
+const name = "0";
+
+if( name.length>0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
