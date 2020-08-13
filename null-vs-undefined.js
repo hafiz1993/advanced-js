4dea3dc6c8b8e5b148c628aab468450ef175d76f
hafiz1993 <hafizurrahaman04@gmail.com>
2020-08-13 22:56:25 +0600

    interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,15 @@
+// let pakhi;
+
+// function add (num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add (num1, num2){
+//     console.log(num1, num2);
+// }
+// const premik = {name:"smart dude", phone:45677};
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
